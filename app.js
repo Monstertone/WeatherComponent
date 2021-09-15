cca4fa757f900dbeed2995b83ea3bea193fb0b5e
Monstertone <bmovie_1999@yahoo.com>
2021-09-14 20:17:52 -0700

    day and night mode now working.  Sunrise and sunset time working

--- app.js
+++ app.js
@@ -12,25 +12,48 @@ app.get('/', async (req, res)=> {
     await axios.get('https://api.openweathermap.org/data/2.5/onecall?lat=33.30&lon=-111.84&units=imperial&appid=ff97ea3bdc0abd998fa94699ae7187cc')
     .then( (report) => {
 
-        // for (let i=0; i<5; i++){
-        // let unixTime = report.data.daily[i].dt
-        // let currentDate = new Date(unixTime * 1000);
-        // let dayOfWeek = currentDate.toLocaleDateString('en-US', {weekday: 'long'});
-        // let newDate = currentDate.toDateString().slice(0,10);
-        // console.log(dayOfWeek)
-        
     
-        // console.log(newDate)
-        // }
-
-        // let weather = report.data.daily;
         let weather = report.data;
+        let sunrise = report.data.current.sunrise;
+        let sunset = report.data.current.sunset;
+        let todaySun =  new Date(sunrise * 1000);
+        let todaySet =  new Date(sunset * 1000);
+        let riseHours = todaySun.getHours();
+        let riseMinutes = todaySun.getMinutes();
+        let setHours = todaySet.getHours()-12;
+        let setMinutes = todaySet.getMinutes();
+        console.log(`today's sunrise is at: ${riseHours}:${riseMinutes}am`)
+        
+        let currentTime = Math.floor(new Date().getTime() / 1000);
 
-        res.render('index.ejs', { weather });
+       
+        console.log(report.data.current.sunrise)
+        console.log(report.data.current.sunset)
 
-        // console.log(report.data.daily[0].temp.day);
-        // console.log(report.data.daily)
-        console.log(report.data.current.weather[0].main)
+       
+
+        let dayMode = (currentTime < sunset ) && (currentTime > sunrise);
+
+        let currentId = weather.current.weather[0].id;
+
+        let changeBgWeather = function(DayOrNight, weatherId) {
+            console.log("It's working!", DayOrNight, weatherId)
+            bgImage = DayOrNight ? "/img/clear.png" : "/img/clear-night.png" ;
+            return bgImage;
+        };
+
+        
+
+        changeBgWeather(dayMode, currentId);
+        
+       
+      console.log(bgImage)
+       
+        
+        res.render('index.ejs', { weather, bgImage, dayMode, riseHours, riseMinutes, setHours, setMinutes });
+
+    
+        console.log(report.data.current.sunrise)
     });
     
 
